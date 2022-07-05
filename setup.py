907c54aced9d840ad63b13eeeab14f11a4db9170
Izzuddin Ahsanujunda <izzuddin.ahsanujunda@dana.id>
2022-07-05 08:19:00 +0700

    CHR: initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+from setuptools import find_namespace_packages, setup
+
+# Load packages from requirements.txt
+BASE_DIR = Path(__file__).parent
+with open(Path(BASE_DIR, "requirements.txt"), "r") as file:
+    required_packages = [ln.strip() for ln in file.readlines()]
+
+# Define our package
+setup(
+    name="xetra_pipeline",
+    version=0.1,
+    description="Data processing pipeline of Xetra data",
+    author="Izzuddin Ahsanujunda",
+    author_email="izzuddin.ahsanujunda@gmail.com",
+    python_requires=">=3.9",
+    packages=find_namespace_packages(),
+    install_requires=[required_packages],
+)
